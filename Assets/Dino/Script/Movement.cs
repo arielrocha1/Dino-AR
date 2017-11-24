e572bbfd7e86af5699dc8169f79e6dd8cd737a74
caiooliveira54 <cdom.snf16@uea.edu.br>
2017-11-23 22:08:06 -0400

    feat: Movimentação teste do Dino

---
+++ Assets/Dino/Script/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public Transform target;
+	public Transform myTransform;
+
+	public float speedMove;
+	//public float speedTarget;
+
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		//direção e distância da transalção
+		//transform.Translate (Vector3.forward * speedMove * Time.deltaTime);
+		//transform.Translate (Vector3.up * Time.deltaTime, Space.World);
+
+		//x com o eixo dos x, y com o eixo dos y, z com o eixo dos z
+		//transform.Translate (0, 0, Time.deltaTime);
+		//transform.Translate (0, Time.deltaTime, 0, Space.World);
+
+		//direção e distância da transalção
+		//transform.Translate (Vector3.right * Time.deltaTime, Camera.main.transform);
+
+		//x com o eixo dos x, y com o eixo dos y, z com o eixo dos z
+		//transform.Translate (Time.deltaTime, 0, 0, Camera.main.transform);
+
+		transform.LookAt (target);
+		transform.Translate (Vector3.forward * 5 * Time.deltaTime);
+
+	}
+}
