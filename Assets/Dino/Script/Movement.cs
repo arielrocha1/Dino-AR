2c39f950a85e064787190accfb22c1a9df4c71d4
caiooliveira54 <cdom.snf16@uea.edu.br>
2017-12-13 15:01:56 -0400

    fix: Dino não mais se move pelos eixos XZ

--- Assets/Dino/Script/Movement.cs
+++ Assets/Dino/Script/Movement.cs
@@ -6,9 +6,10 @@ public class Movement : MonoBehaviour {
 
 	public Transform target;
 	private Animator anime;
+	//private Rigidbody rb;
 
 	public float speedMove;
-	public float turnRate;
+	//public float turnRate;
 	private int lockAxis = 0;
 	//public float speedTarget;
 
@@ -17,6 +18,7 @@ public class Movement : MonoBehaviour {
 
 		anime = GetComponent<Animator>();
 		target = GameObject.FindGameObjectWithTag("Target").transform;
+		//rb = GetComponent<Rigidbody>();
 
 	}
 
@@ -34,21 +36,12 @@ public class Movement : MonoBehaviour {
 	void Walk(){
 		//transform.Translate (Vector3.forward * speedMove * Time.deltaTime);
 
-		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (target.position - transform.position), speedMove * Time.deltaTime);
-		//transform.rotation = Quaternion.Euler(lockAxis, transform.rotation.eulerAngles.y, lockAxis);
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (target.position - transform.position), speedMove * Time.deltaTime); //rotaciona o Dino pro alvo
+		transform.rotation = Quaternion.Euler(lockAxis, transform.rotation.eulerAngles.y, lockAxis); // anula os eixos XZ
 
 		transform.position += transform.forward * speedMove * Time.deltaTime;
 		anime.Play("Walk");
 	}
-
-	/*void Rotate(){
-
-		Vector3 relativePos = (target.position + new Vector3 (0, 1.5f, 0)) - transform.position;
-		Quaternion rotation = Quaternion.LookRotation (relativePos);
-
-		Quaternion current = transform.localRotation;
-		var rot = Quaternion.Slerp (current, rotation, Time.deltaTime);
-		transform.localRotation;
-	}*/
+		
 
 }
